b4ce9ea32785b681bbd35f927291c4d0b37579dc
Yuzuru <yuzuru@programmer.net>
2023-06-06 14:54:57 -0500

    minor fixes

--- dist/mixer.d.ts
+++
@@ -1,4 +0,0 @@
-type ConstructorAux<T extends abstract new (..._args: any) => InstanceType<T>> = new (..._args: ConstructorParameters<T>) => InstanceType<T>;
-export declare function mix<T extends ConstructorAux<T>, U extends ConstructorAux<T>>(structToApply: T, struct: U, ignore?: Array<keyof T['prototype']>): Struct<T, U>;
-export type Struct<ToMix = {}, Final = {}> = Final extends new (..._args: never[]) => infer F ? ToMix extends new (..._args: never[]) => infer TM ? new (..._args: ConstructorParameters<Final>) => F & TM : never : never;
-export {};
